5c4ba1f8e8ae7816a709075a6034b7007e60d6f1
Doan Trong Duong <duongdoan2k6@gmail.com>
2025-11-14 01:48:31 +0700

    update

---
+++ main.js
@@ -0,0 +1,9 @@
+
+const phepTinh = (a, b) => {
+    return a + b;
+}
+
+const noiHienThi = document.getElementById("ket-qua");
+noiHienThi.innerHTML = phepTinh(5, 10);
+
+console.log(phepTinh(5, 10));
